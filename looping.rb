e72c320f8136dd5902c497bcc823c13a6d2b477c
Ronaldkubasu <ronald.kubasu@student.moringaschool.com>
2022-10-19 14:42:02 +0300

    Completed Assignment

--- looping.rb
+++ looping.rb
@@ -1,5 +1,9 @@
 def happy_new_year
   # your code here
+  i = 0
+while i < 10
+  puts "Happy New Year!"
+  i += 1
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,6 +21,10 @@ end
 
 def fizzbuzz_printer
   # your code here
+
+end100.times do |i|
+  print "Fizz!"
+  puts "i is: #{i}"
 end
 
 def reverse_string(str)
